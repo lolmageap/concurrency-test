80f0e20e5855243b19a4629c6d12d4f579bcd638
cherhy <ekxk1234@naver.com>
2024-01-05 23:44:30 +0900

    create database and transaction config

---
+++ src/main/kotlin/com/example/concurrency/DatabaseProperty.kt
@@ -0,0 +1,8 @@
+package com.example.concurrency
+
+object DatabaseProperty {
+    const val MASTER_DATASOURCE = "masterDataSource"
+    const val SLAVE_DATASOURCE = "slaveDataSource"
+    const val SLAVE = "slave"
+    const val MASTER = "master"
+}
---
+++ src/main/kotlin/com/example/concurrency/TransactionConfig.kt
@@ -0,0 +1,68 @@
+package com.example.concurrency
+
+import com.example.concurrency.DatabaseProperty.MASTER
+import com.example.concurrency.DatabaseProperty.MASTER_DATASOURCE
+import com.example.concurrency.DatabaseProperty.SLAVE
+import com.example.concurrency.DatabaseProperty.SLAVE_DATASOURCE
+import com.zaxxer.hikari.HikariDataSource
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.DependsOn
+import org.springframework.context.annotation.Primary
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource
+import org.springframework.transaction.support.TransactionSynchronizationManager
+import javax.sql.DataSource
+
+@Configuration
+class TransactionConfig {
+
+    @Bean(name = [MASTER_DATASOURCE])
+    @ConfigurationProperties(prefix = "spring.datasource.master.hikari")
+    fun masterDataSource(): HikariDataSource =
+        DataSourceBuilder.create()
+            .type(HikariDataSource::class.java)
+            .build()
+
+    @Bean(name = [SLAVE_DATASOURCE])
+    @ConfigurationProperties("spring.datasource.slave.hikari")
+    fun slaveDataSource(): HikariDataSource =
+        DataSourceBuilder.create()
+            .type(HikariDataSource::class.java)
+            .build()
+            .apply { this.isReadOnly = true }
+
+    @Bean
+    @DependsOn(MASTER_DATASOURCE, SLAVE_DATASOURCE)
+    fun routingDataSource(
+        @Qualifier(MASTER_DATASOURCE) masterDataSource: DataSource,
+        @Qualifier(SLAVE_DATASOURCE) slaveDataSource: DataSource,
+    ): DataSource {
+        val dataSources = hashMapOf<Any, Any>().apply {
+            this[MASTER] = masterDataSource
+            this[SLAVE] = slaveDataSource
+        }
+
+        return RoutingDataSource().apply {
+            setTargetDataSources(dataSources)
+            setDefaultTargetDataSource(masterDataSource)
+        }
+    }
+
+    @Bean
+    @Primary
+    @DependsOn("routingDataSource")
+    fun dataSource(routingDataSource: DataSource) =
+        LazyConnectionDataSourceProxy(routingDataSource)
+}
+
+class RoutingDataSource : AbstractRoutingDataSource() {
+    override fun determineCurrentLookupKey(): Any =
+        when {
+            TransactionSynchronizationManager.isCurrentTransactionReadOnly() -> SLAVE
+            else -> MASTER
+        }
+}
