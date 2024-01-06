1f653cce73089433bc95fb80014f43aa46fc79ea
cherhy <ekxk1234@naver.com>
2024-01-06 14:58:38 +0900

    create test extension

---
+++ src/test/kotlin/com/example/concurrency/TestExtension.kt
@@ -0,0 +1,23 @@
+package com.example.concurrency
+
+import com.example.concurrency.Prefix.EMPTY
+import io.kotest.core.test.TestCase
+
+val TestCase.prefix: Prefix
+    get() {
+        val prefix = this.parent?.name?.prefix ?: return EMPTY
+        return Prefix.convert(prefix)
+    }
+
+enum class Prefix {
+    GIVEN, WHEN, THEN, EMPTY;
+    companion object {
+        fun convert(value: String) =
+            when(value.lowercase()) {
+                "given" -> GIVEN
+                "when" -> WHEN
+                "then" -> THEN
+                else -> EMPTY
+            }
+    }
+}
