205360bb8e0695bc072852b5d0afcd328c2bf599
lolmageap <ekxk1234@naver.com>
2024-01-13 23:14:38 +0900

    add transaction route test

---
+++ src/test/kotlin/com/example/concurrency/TransactionRouteTest.kt
@@ -0,0 +1,33 @@
+package com.example.concurrency
+
+import com.example.concurrency.rdbms.ConCurrencyRepository
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
+
+@SpringBootTest
+class TransactionRouteTest(
+    @Autowired private val useCase: UseCase,
+    @Autowired private val conCurrencyRepository: ConCurrencyRepository,
+): StringSpec({
+
+    beforeEach {
+        conCurrencyRepository.saveAndFlush(
+            ConCurrencyEntity(
+                id = 1,
+                name = "Test",
+            )
+        )
+    }
+
+    "read slave and write master" {
+        useCase.readAndWrite(1, "Updated")
+
+        val result = conCurrencyRepository.findByIdOrNull(1)!!
+        result.name shouldBe "Updated"
+    }
+
+
+})
