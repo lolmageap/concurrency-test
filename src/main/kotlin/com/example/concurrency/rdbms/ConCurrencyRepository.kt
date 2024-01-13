80f5e418f48d37d1e73e42c27613850ae9d76934
lolmageap <ekxk1234@naver.com>
2024-01-13 23:14:10 +0900

    add modifying query

--- src/main/kotlin/com/example/concurrency/rdbms/ConCurrencyRepository.kt
+++ src/main/kotlin/com/example/concurrency/rdbms/ConCurrencyRepository.kt
@@ -4,8 +4,15 @@ import com.example.concurrency.ConCurrencyEntity
 import jakarta.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Modifying
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 interface ConCurrencyRepository : JpaRepository<ConCurrencyEntity, Long> {
     @Lock(LockModeType.PESSIMISTIC_WRITE)
     fun findWithPessimisticLockById(id: Long): ConCurrencyEntity?
+
+    @Modifying
+    @Query("update ConCurrencyEntity set name = :name where id = :id")
+    fun updateForce(@Param("id") id: Long, @Param("name") name: String)
 }
