63910816ee1ccba2c9dbd637f822c483dd4a6d37
lolmageap <ekxk1234@naver.com>
2024-03-22 23:58:12 +0900

    presentation dirty checking example

--- src/test/kotlin/com/example/concurrency/mvc/UseCaseTest.kt
+++ src/test/kotlin/com/example/concurrency/mvc/UseCaseTest.kt
@@ -3,7 +3,9 @@ package com.example.concurrency.mvc
 import com.example.concurrency.ConcurrencyEntity
 import com.example.concurrency.rdbms.ConcurrencyRepository
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.data.repository.findByIdOrNull
 
 @SpringBootTest
 class UseCaseTest(
@@ -18,5 +20,6 @@ class UseCaseTest(
         )
 
         useCase.transactionTest2(entity.id)
+        concurrencyRepository.findByIdOrNull(entity.id)!!.name shouldBe "second"
     }
 })
